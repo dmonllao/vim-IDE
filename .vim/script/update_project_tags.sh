14cdfb84da37482812cb5c23e7d0e31562fe45b1
David Monllao <davidm@moodle.com>
2012-11-29 13:59:48 +0800

    Moving project references to ~/.vim

--- .vim/script/update_project_tags.sh
+++ .vim/script/update_project_tags.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-# Lists all the PHP files of the project and
-find $PWD/* -name '*.php' > $PWD/cscope.files
+# Tmp path to store the .php files list.
+CSCOPELIST=/tmp/cscope_`date +%s`
 
-# Creates cscope.out with the definitions list
-cscope -b
-rm ./cscope.files
+# Lists all the PHP files of the project scope.
+find $PWD/* -name '*.php' > $CSCOPELIST
+
+# File name depending on $PWD.
+CSCOPEOUT=`md5sum <<EOF
+$PWD
+EOF`
+
+# Path to store the cscope references file.
+# The name of the var is set as expected by cscope to load it.
+CSCOPE_DB=~/.vim/cscope_databases/$CSCOPEOUT
+export $CSCOPE_DB
+
+# Creates cscope.out with the references list.
+cscope -b -f $CSCOPE_DB $CSCOPELIST
+
+# Delete tmp file.
+rm $CSCOPELIST
