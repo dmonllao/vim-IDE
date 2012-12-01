ff63b92a380aec557c85856cf9d0d9edcc65222d
David Monllao <david.monllao@gmail.com>
2012-12-01 22:17:58 +0800

    cscope.files renaming
    Also solved problem with tmp/ availability

--- .vim/script/update_project_tags.sh
+++ .vim/script/update_project_tags.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-# Tmp path to store the .php files list.
-CSCOPELIST=/tmp/cscope_`date +%s`
-
-# Lists all the PHP files of the project scope.
-find $PWD/* -name '*.php' > $CSCOPELIST
-
 # File name depending on $PWD.
 PWDHASH=`md5sum <<EOF
 $PWD
 EOF`
 CSCOPEOUT="${PWDHASH%% *}"
 
+# Tmp path to store the .php files list.
+CSCOPELIST=~/.vim/cscope_files/$CSCOPEOUT
+
+# Lists all the PHP files of the project scope.
+find $PWD/* -name '*.php' > $CSCOPELIST
+
 # Path to store the cscope references file.
 # The name of the var is set as expected by cscope to load it.
 CSCOPE_DB=~/.vim/cscope_databases/$CSCOPEOUT
 
 # Creates cscope.out with the references list.
-cscope -b -f $CSCOPE_DB $CSCOPELIST
+cscope -b -i $CSCOPELIST -f $CSCOPE_DB $CSCOPELIST
 
 # Delete tmp file.
 rm $CSCOPELIST
